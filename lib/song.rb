f9bab1542358835fc631e8d453451049370da95e
McKayla Lankau <lankaukk@yahoo.com>
2020-10-21 02:06:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,6 +39,15 @@ class Song
     genre.add_song(self)
   end
   
+  def self.find_by_name(name)
+    @@all.find do |song|
+        song.name == name
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end
   
 end
 
