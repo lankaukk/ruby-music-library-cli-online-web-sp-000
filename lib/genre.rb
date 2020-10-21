30226570f91df915bbc2e5ed37bb5c2b66868666
McKayla Lankau <lankaukk@yahoo.com>
2020-10-21 01:49:01 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -41,4 +41,16 @@ class Genre
     end
     song
   end
+  
+  def genres
+    @new_array = []
+    @songs.each do |song|
+      if @new_array.include?(song.artist)
+        nil
+      else
+        @new_array << song.artist
+      end
+    end
+    @new_array
+  end
 end
