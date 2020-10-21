17ad3ce94b731f6d843587064a4059fe599255f3
McKayla Lankau <lankaukk@yahoo.com>
2020-10-21 02:31:35 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -118,6 +118,8 @@ class MusicLibraryController
     if (1..Song.all.length).include?(input)
       song = list_of_songs[input+2]
       puts "Playing #{song.name} by #{song.artist.name}"
+    else
+      nil
     end
   end
 
